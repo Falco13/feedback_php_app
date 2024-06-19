2a58fc402fd835c0090f2380856ee2f3728b6e2d
Falco13 <keanu0608@gmail.com>
2024-06-19 14:44:53 +0300

    layout, about, home page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,28 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About page</title>
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="">PHP App</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="">Home</a>
-                <a class="nav-link" href="">Contact</a>
-                <a class="nav-link" href="">About</a>
-            </div>
-        </div>
-    </div>
-</nav>
-    <h1>Page about</h1>
-</body>
-</html>
+@extends('layout')
+
+@section('main_content')
+
+<h1>Page about</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Route::get('/hello', function() {
-    return "Hello php-world!";
+Route::get('/', function() {
+    return view('layout');
 });
 
 Route::get('/about', function() {
