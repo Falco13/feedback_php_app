0a05789a53d4b31773404ffc130375c0814f49c5
Falco13 <keanu0608@gmail.com>
2024-06-19 16:40:44 +0300

    make controller

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () {
 //    return view('welcome');
 //});
 
-Route::get('/', function() {
-    return view('home');
-});
+Route::get('/', [MainController::class, 'home']);
 
+Route::get('/about', [MainController::class, 'about']);
+
+/*
 Route::get('/about', function() {
     return view('about');
 });
+*/
