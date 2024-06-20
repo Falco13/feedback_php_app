cc8a97855bd2fb74a31c2c9ee91b1da867fd9ac1
Falco13 <keanu0608@gmail.com>
2024-06-20 15:00:26 +0300

    contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,18 @@
 @section('title') Contact @endsection
 @section('main_content')
 
-<h1>Contact Page</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-7 m-auto">
+            <h1>Contact Page</h1>
+            <form method="post" action="contact">
+                <input name="email" type="email" id="email" placeholder="Your email" class="form-control"><br>
+                <input name="subject" type="text" id="subject" placeholder="Your subject" class="form-control"><br>
+                <textarea name="message" id="message" placeholder="Your message" class="form-control"></textarea><br>
+                <button type="submit" class="btn btn-warning">Send message</button>
+            </form>
+        </div>
+    </div>
+</div>
 
 @endsection
