ea37fc1bc1fbfc1bd31af5417b78840a61f6a410
Falco13 <keanu0608@gmail.com>
2024-06-20 13:39:17 +0300

    code stripping

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,7 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
 
 Route::get('/', [MainController::class, 'home']);
 
 Route::get('/about', [MainController::class, 'about']);
-
-/*
-Route::get('/about', function() {
-    return view('about');
-});
-*/
