5fbe87f620e994a599c1542f5a62e44ccf8cc3f7
Falco13 <keanu0608@gmail.com>
2024-06-21 16:31:03 +0300

    contactmodel, route name, migrate

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ContactModel;
 use Illuminate\Http\Request;
 
 class MainController extends Controller {
@@ -24,5 +25,12 @@ class MainController extends Controller {
             'subject' => 'required|min:4|max:50',
             'message' => 'required|min:15|max:500',
         ]);
+        $contact = new ContactModel();
+        $contact->email = $request->input('email');
+        $contact->subject = $request->input('subject');
+        $contact->message = $request->input('message');
+
+        $contact->save();
+        return redirect()->route('contact');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,5 @@ Route::get('/', [MainController::class, 'home']);
 
 Route::get('/about', [MainController::class, 'about']);
 
-Route::get('/contact', [MainController::class, 'contact']);
+Route::get('/contact', [MainController::class, 'contact'])->name('contact');
 Route::post('/contact/check', [MainController::class, 'contact_check']);
